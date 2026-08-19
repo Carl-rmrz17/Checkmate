fc1b52d9b827c0a56c0d59338ac33add63c75337
John Carlo Jamolo <johncarlorjamolo@iskolarngbayan.pup.edu.ph>
2026-08-20 00:04:11 +0800

    Update config.js with Supabase credentials

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = 'YOUR_SUPABASE_PROJECT_URL';
-export const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+export const SUPABASE_URL = 'https://phvrhzbjrnlywjkqqmdu.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_O5BPL_ZGCVumlMKzc3cHHw_JZQKgOVx';
